d71d36c0bfc7fecce822bf9fd39ce9d6fe922ef1
Margherita <margherita.testi@gmail.com>
2020-09-02 11:22:35 +0200

    esercizio.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.header-right .fas fa-bars').click (
+  function () {
+    $(".hamburger-menu").show();
+  }
+);
+
+$(".hamburger-menu .close").click (
+  function (){
+    $(".hamburger-menu").hide();
+  };
+)
